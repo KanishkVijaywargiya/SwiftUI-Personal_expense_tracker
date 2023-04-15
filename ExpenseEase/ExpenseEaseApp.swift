a932ab9cae51c8b6560b265a6984b3b5da99409f
KanishkVijaywargiya <secretcodersociety@gmail.com>
2023-04-15 17:35:57 +0530

    Initial Commit

---
+++ ExpenseEase/ExpenseEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseEaseApp.swift
+//  ExpenseEase
+//
+//  Created by Kanishk Vijaywargiya on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
